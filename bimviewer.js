1aa27ac7971eb0c039ea4dd289b5241619fe5594
jlp <mattlukin@gmail.com>
2022-08-18 16:59:32 +0100

    fixed html elements visibility, added filter (hardcoded), added svgs

--- bimviewer.js
+++ bimviewer.js
@@ -2,7 +2,28 @@ import { Color } from "three";
 import { IfcViewerAPI } from "web-ifc-viewer";
 import { modelName, toolbar, checkboxes } from "./overlay.js";
 
+import {
+    IFCWALLSTANDARDCASE,
+    IFCSLAB,
+    IFCDOOR,
+    IFCWINDOW,
+    IFCFURNISHINGELEMENT,
+    IFCMEMBER,
+    IFCPLATE
+  } from 'web-ifc';
+
 const socketiourl = "http://192.168.236.229:8088/"; //edit socket.io url here
+
+// List of categories names
+const categories = {
+    IFCWALLSTANDARDCASE,
+    IFCSLAB,
+    IFCFURNISHINGELEMENT,
+    IFCDOOR,
+    IFCWINDOW,
+    IFCPLATE,
+    IFCMEMBER
+  };
 
 const container = document.getElementById("viewer-container");
 const viewer = new IfcViewerAPI({
@@ -37,7 +58,13 @@ const socket = io(socketiourl);
 
 //socket.emit("createProject", "createProject");//create project
 
-socket.emit("getLatestRevision", currentProjectID);
+
+//otherwise socket app will crash 
+if (currentProjectID !== null)
+    {
+        console.log("getting latest revision", currentProjectID);
+        socket.emit("getLatestRevision", currentProjectID);
+    }
 
 async function loadIfc(url) {
   // Load the model
@@ -47,7 +74,11 @@ async function loadIfc(url) {
   await viewer.shadowDropper.renderShadow(model.modelID);
   viewer.context.renderer.postProduction.active = true;
 
+  model.removeFromParent(); //for ifc categories filter
+
   const ifcProject = await viewer.IFC.getSpatialStructure(model.modelID);
+
+  await setupAllCategories(); //for ifc categories filter
   createTreeMenu(ifcProject);
 }
 
@@ -57,6 +88,9 @@ socket.on("fileName", (fileName) => {
   //console.log(path);
 });
 
+
+const scene = viewer.context.getScene(); //for showing/hiding categories
+
 //  let path = "http://localhost:8088/testmodel.ifc"; // get path into this
 //  loadIfc(path);
 
@@ -65,18 +99,13 @@ socket.on("fileName", (fileName) => {
 //get ifc properties, need to add the html element etc..
 const propsGUI = document.getElementById("ifc-property-menu-root");
 
+document.getElementById("ifc-property-menu").style.display = "none";
+document.getElementById("ifc-tree-menu").style.display = "none";
+document.getElementById("checkboxes").style.display = "none";
 
 //modelName("Test IFC model"); //need to get the model name from bimserver - from project id? - call getProjects socket event, get the name from list of ids where id is in url
 
-//checkboxes();
-
-// svgPaths = [
-//   "M21.172 24l-7.387-7.387c-1.388.874-3.024 1.387-4.785 1.387-4.971 0-9-4.029-9-9s4.029-9 9-9 9 4.029 9 9c0 1.761-.514 3.398-1.387 4.785l7.387 7.387-2.828 2.828zm-12.172-8c3.859 0 7-3.14 7-7s-3.141-7-7-7-7 3.14-7 7 3.141 7 7 7zm-3-8c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1zm3 0c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1zm3 0c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1z",
-//   "M18 10.031v-6.423l-6.036-3.608-5.964 3.569v6.499l-6 3.224v7.216l6.136 3.492 5.864-3.393 5.864 3.393 6.136-3.492v-7.177l-6-3.3zm-1.143.036l-4.321 2.384v-4.956l4.321-2.539v5.111zm-4.895-8.71l4.272 2.596-4.268 2.509-4.176-2.554 4.172-2.551zm-10.172 12.274l4.778-2.53 4.237 2.417-4.668 2.667-4.347-2.554zm4.917 3.587l4.722-2.697v5.056l-4.722 2.757v-5.116zm6.512-3.746l4.247-2.39 4.769 2.594-4.367 2.509-4.649-2.713zm9.638 6.323l-4.421 2.539v-5.116l4.421-2.538v5.115z",
-//   "M14.686 13.646l-6.597 3.181c-1.438.692-2.755-1.124-2.755-1.124l6.813-3.287 2.539 1.23zm6.168 5.354c-.533 0-1.083-.119-1.605-.373-1.511-.731-2.296-2.333-1.943-3.774.203-.822-.23-.934-.891-1.253l-11.036-5.341s1.322-1.812 2.759-1.117c.881.427 4.423 2.136 7.477 3.617l.766-.368c.662-.319 1.094-.43.895-1.252-.351-1.442.439-3.043 1.952-3.77.521-.251 1.068-.369 1.596-.369 1.799 0 3.147 1.32 3.147 2.956 0 1.23-.766 2.454-2.032 3.091-1.266.634-2.15.14-3.406.75l-.394.19.431.21c1.254.614 2.142.122 3.404.759 1.262.638 2.026 1.861 2.026 3.088 0 1.64-1.352 2.956-3.146 2.956zm-1.987-9.967c.381.795 1.459 1.072 2.406.617.945-.455 1.405-1.472 1.027-2.267-.381-.796-1.46-1.073-2.406-.618-.946.455-1.408 1.472-1.027 2.268zm-2.834 2.819c0-.322-.261-.583-.583-.583-.321 0-.583.261-.583.583s.262.583.583.583c.322.001.583-.261.583-.583zm5.272 2.499c-.945-.457-2.025-.183-2.408.611-.381.795.078 1.814 1.022 2.271.945.458 2.024.184 2.406-.611.382-.795-.075-1.814-1.02-2.271zm-18.305-3.351h-3v2h3v-2zm4 0h-3v2h3v-2z",
-//   "M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z",
-//   "M1.438 16.873l-1.438 7.127 7.127-1.437 16.874-16.872-5.69-5.69-16.873 16.872zm1.12 4.572l.722-3.584 2.86 2.861-3.582.723zm18.613-15.755l-13.617 13.617-2.86-2.861 13.617-13.617 2.86 2.861z",
-// ];
+//checkboxes(); //this is not working
 
 toolbar();
 
@@ -90,6 +119,8 @@ window.ondblclick = async () => {
   const props = await viewer.IFC.getProperties(modelID, id, true, false); //can also use getTypeProperties(), getMaterialProperties() and getPropertySets()
   console.log(props); //call the function here
   createPropertiesMenu(props);
+  document.getElementById("ifc-property-menu").style.display = "initial";
+  propertiesButton.classList.add("active");
 };
 
 //set up clipping planes - try to put inside function (see three model loader - switch views button)
@@ -130,6 +161,64 @@ for (let i = 0; i < toggler.length; i++) {
         toggler[i].classList.toggle("caret-down");
     }
 }
+
+// hiding/filters
+
+
+  
+  // Gets the name of a category
+  function getName(category) {
+    const names = Object.keys(categories);
+    return names.find(name => categories[name] === category);
+  }
+  
+// Gets all the items of a category
+async function getAll(category) {
+	return viewer.IFC.loader.ifcManager.getAllItemsOfType(0, category, false);
+}
+  
+  // Creates a new subset containing all elements of a category
+async function newSubsetOfType(category) {
+	const ids = await getAll(category);
+	return viewer.IFC.loader.ifcManager.createSubset({
+		modelID: 0,
+		scene,
+		ids,
+		removePrevious: true,
+		customID: category.toString(),
+	});
+}
+
+// Stores the created subsets
+const subsets = {};
+  
+  async function setupAllCategories() {
+      const allCategories = Object.values(categories);
+      for (let i = 0; i < allCategories.length; i++) {
+          const category = allCategories[i];
+          await setupCategory(category);
+      }
+  }
+  
+  // Creates a new subset and configures the checkbox
+  async function setupCategory(category) {
+      subsets[category] = await newSubsetOfType(category);
+      setupCheckBox(category);
+  }
+  
+  // Sets up the checkbox event to hide / show elements
+  function setupCheckBox(category) {
+      const name = getName(category);
+      const checkBox = document.getElementById(name);
+      checkBox.addEventListener('change', (event) => {
+          const checked = event.target.checked;
+          const subset = subsets[category];
+          if (checked) scene.add(subset);
+          else subset.removeFromParent();
+      });
+  }
+  
+
 
 // Spatial tree menu
 
@@ -192,16 +281,15 @@ function createSimpleChild(parent, node) {
     }
 
     childNode.onclick = async () => {
-        const res = viewer.IFC.selector.pickIfcItemsByID(0, [node.expressID]);
-        //console.log(res);
-        //display properties
-        //console.log(viewer.IFC.selector.pickIfcItemsByID(0, [node.expressID]));
-       // const result = await viewer.IFC.selector.pickIfcItem(); //highlightIfcItem hides all other elements
-        if (!result) return;
-        const { modelID, id } = result;
-        const props = await viewer.IFC.getProperties(0, id, true, false); //can also use getTypeProperties(), getMaterialProperties() and getPropertySets()
+        viewer.IFC.selector.pickIfcItemsByID(0, [node.expressID], true);
+
+        let idsArray = [node.expressID];
+
+        const props = await viewer.IFC.getProperties(0, idsArray[0], true, false); //can also use getTypeProperties(), getMaterialProperties() and getPropertySets()
         console.log(props); //call the function here
         createPropertiesMenu(props);
+        document.getElementById("ifc-property-menu").style.display = "initial";
+        propertiesButton.classList.add("active");
     }
 }
 
--- overlay.js
+++ overlay.js
@@ -1,7 +1,5 @@
 //UI functions
 
-import { IfcViewerAPI } from "web-ifc-viewer";
-
 const categoriesText = [
     'IFCWALLSTANDARDCASE',
     'IFCSLAB',
@@ -12,11 +10,6 @@ const categoriesText = [
     'IFCMEMBER'
   ]
 
-  svgPaths = ["M21.172 24l-7.387-7.387c-1.388.874-3.024 1.387-4.785 1.387-4.971 0-9-4.029-9-9s4.029-9 9-9 9 4.029 9 9c0 1.761-.514 3.398-1.387 4.785l7.387 7.387-2.828 2.828zm-12.172-8c3.859 0 7-3.14 7-7s-3.141-7-7-7-7 3.14-7 7 3.141 7 7 7zm-3-8c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1zm3 0c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1zm3 0c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1z",
-"M18 10.031v-6.423l-6.036-3.608-5.964 3.569v6.499l-6 3.224v7.216l6.136 3.492 5.864-3.393 5.864 3.393 6.136-3.492v-7.177l-6-3.3zm-1.143.036l-4.321 2.384v-4.956l4.321-2.539v5.111zm-4.895-8.71l4.272 2.596-4.268 2.509-4.176-2.554 4.172-2.551zm-10.172 12.274l4.778-2.53 4.237 2.417-4.668 2.667-4.347-2.554zm4.917 3.587l4.722-2.697v5.056l-4.722 2.757v-5.116zm6.512-3.746l4.247-2.39 4.769 2.594-4.367 2.509-4.649-2.713zm9.638 6.323l-4.421 2.539v-5.116l4.421-2.538v5.115z",
-"M14.686 13.646l-6.597 3.181c-1.438.692-2.755-1.124-2.755-1.124l6.813-3.287 2.539 1.23zm6.168 5.354c-.533 0-1.083-.119-1.605-.373-1.511-.731-2.296-2.333-1.943-3.774.203-.822-.23-.934-.891-1.253l-11.036-5.341s1.322-1.812 2.759-1.117c.881.427 4.423 2.136 7.477 3.617l.766-.368c.662-.319 1.094-.43.895-1.252-.351-1.442.439-3.043 1.952-3.77.521-.251 1.068-.369 1.596-.369 1.799 0 3.147 1.32 3.147 2.956 0 1.23-.766 2.454-2.032 3.091-1.266.634-2.15.14-3.406.75l-.394.19.431.21c1.254.614 2.142.122 3.404.759 1.262.638 2.026 1.861 2.026 3.088 0 1.64-1.352 2.956-3.146 2.956zm-1.987-9.967c.381.795 1.459 1.072 2.406.617.945-.455 1.405-1.472 1.027-2.267-.381-.796-1.46-1.073-2.406-.618-.946.455-1.408 1.472-1.027 2.268zm-2.834 2.819c0-.322-.261-.583-.583-.583-.321 0-.583.261-.583.583s.262.583.583.583c.322.001.583-.261.583-.583zm5.272 2.499c-.945-.457-2.025-.183-2.408.611-.381.795.078 1.814 1.022 2.271.945.458 2.024.184 2.406-.611.382-.795-.075-1.814-1.02-2.271zm-18.305-3.351h-3v2h3v-2zm4 0h-3v2h3v-2z",
-"M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z",
-"M1.438 16.873l-1.438 7.127 7.127-1.437 16.874-16.872-5.69-5.69-16.873 16.872zm1.12 4.572l.722-3.584 2.86 2.861-3.582.723zm18.613-15.755l-13.617 13.617-2.86-2.861 13.617-13.617 2.86 2.861z"]
 
 
 //needs to be fixed, it is over other html elements!
@@ -38,6 +31,7 @@ function button (svgpath) {
     const button = document.createElement('a');
     button.className = 'button';
     button.href= './bimviewer.html'; //also needs to be an input
+    
   
     const svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svgEl.setAttribute('width', '15');
@@ -154,10 +148,11 @@ export function createCardDiv(projectName, projectId) {
 // }
 function homeButton() {
 
-    const homeButton= document.createElement("a"); //should these be buttons instead of 'a'?
+    const homeButton= document.createElement("button"); //should these be buttons instead of 'a'?
     homeButton.className = "button";
 
-    homeButton.href = "./index.html";
+   // homeButton.href = "./index.html";
+    homeButton.setAttribute("onclick","window.location.href='./index.html';");
     //filterButton.
 
     const svgEl = document.createElementNS("http://www.w3.org/2000/svg", "svg");
@@ -166,7 +161,7 @@ function homeButton() {
     svgEl.setAttribute("viewBox", "0 0 24 24");
 
     const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path1.setAttribute("d", svgPaths[0]); //change icon here
+    path1.setAttribute("d", "M6 7v-7h13v10h5v14h-23v-17h5zm0 16v-4h-1v4h1zm8-4h-3v4h3v-4zm6 0h-1v4h1v-4zm2-7h-3v6h2v4h1v-10zm-5-10h-9v20h1v-5h7v5h1v-20zm-13 20v-4h2v-9h-3v13h1zm17-6h-1v-2h1v2zm-17-2h1v2h-1v-2zm8 1h-2v-2h2v2zm3 0h-2v-2h2v2zm-10-4v2h-1v-2h1zm7 1h-2v-2h2v2zm3 0h-2v-2h2v2zm-3-3h-2v-2h2v2zm3 0h-2v-2h2v2zm-3-3h-2v-2h2v2zm3 0h-2v-2h2v2z"); //change icon here
 
     svgEl.appendChild(path1);
     homeButton.appendChild(svgEl);
@@ -186,10 +181,26 @@ function filterButton() { //need individual class names for buttons?
     svgEl.setAttribute("viewBox", "0 0 24 24");
 
     const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path1.setAttribute("d", svgPaths[0]); //add svg
+    path1.setAttribute("d", "M19.479 2l-7.479 12.543v5.924l-1-.6v-5.324l-7.479-12.543h15.958zm3.521-2h-23l9 15.094v5.906l5 3v-8.906l9-15.094z"); //add svg
 
     svgEl.appendChild(path1);
     filterButton.appendChild(svgEl);
+
+    filterButton.addEventListener("click", function() {
+ 
+         if (document.getElementById("checkboxes").style.display === "initial")
+         {
+             document.getElementById("checkboxes").style.display = "none";
+             filterButton.classList.remove("active");
+         }
+         else if (document.getElementById("checkboxes").style.display === "none")
+         {
+             document.getElementById("checkboxes").style.display = "initial";
+             filterButton.classList.add("active");
+         }
+         
+         
+         }, false);
 
     return filterButton;
 
@@ -207,7 +218,7 @@ function propertiesButton() { //toggle on off properties
     svgEl.setAttribute("viewBox", "0 0 24 24");
 
     const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path1.setAttribute("d", svgPaths[1]);
+    path1.setAttribute("d", "M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z");
 
     svgEl.appendChild(path1);
     propertiesButton.appendChild(svgEl);
@@ -216,15 +227,30 @@ function propertiesButton() { //toggle on off properties
     let mouseDown = false;
     //events to switch between views
     propertiesButton.addEventListener("click", function() {
-        if (mouseDown === false) {
-            mouseDown = true;
+       // document.getElementById("ifc-property-menu").style.display = "initial";
+
+        // if (mouseDown === false) {
+        //     mouseDown = true;
+        //     document.getElementById("ifc-property-menu").style.display = "initial";
+        //     propertiesButton.classList.add("active");
+        // } else if (mouseDown === true) {
+        //     mouseDown = false;
+        //     document.getElementById("ifc-property-menu").style.display = "none";
+        //     propertiesButton.classList.remove("active");
+        // }
+
+        if (document.getElementById("ifc-property-menu").style.display === "initial")
+        {
             document.getElementById("ifc-property-menu").style.display = "none";
             propertiesButton.classList.remove("active");
-        } else if (mouseDown === true) {
-            mouseDown = false;
+        }
+        else if (document.getElementById("ifc-property-menu").style.display === "none")
+        {
             document.getElementById("ifc-property-menu").style.display = "initial";
             propertiesButton.classList.add("active");
         }
+        
+        
         }, false);
 
     return propertiesButton;
@@ -243,7 +269,7 @@ function clipPlaneButton() {
     svgEl.setAttribute("viewBox", "0 0 24 24");
 
     const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path1.setAttribute("d", svgPaths[2]);
+    path1.setAttribute("d", "M14.686 13.646l-6.597 3.181c-1.438.692-2.755-1.124-2.755-1.124l6.813-3.287 2.539 1.23zm6.168 5.354c-.533 0-1.083-.119-1.605-.373-1.511-.731-2.296-2.333-1.943-3.774.203-.822-.23-.934-.891-1.253l-11.036-5.341s1.322-1.812 2.759-1.117c.881.427 4.423 2.136 7.477 3.617l.766-.368c.662-.319 1.094-.43.895-1.252-.351-1.442.439-3.043 1.952-3.77.521-.251 1.068-.369 1.596-.369 1.799 0 3.147 1.32 3.147 2.956 0 1.23-.766 2.454-2.032 3.091-1.266.634-2.15.14-3.406.75l-.394.19.431.21c1.254.614 2.142.122 3.404.759 1.262.638 2.026 1.861 2.026 3.088 0 1.64-1.352 2.956-3.146 2.956zm-1.987-9.967c.381.795 1.459 1.072 2.406.617.945-.455 1.405-1.472 1.027-2.267-.381-.796-1.46-1.073-2.406-.618-.946.455-1.408 1.472-1.027 2.268zm-2.834 2.819c0-.322-.261-.583-.583-.583-.321 0-.583.261-.583.583s.262.583.583.583c.322.001.583-.261.583-.583zm5.272 2.499c-.945-.457-2.025-.183-2.408.611-.381.795.078 1.814 1.022 2.271.945.458 2.024.184 2.406-.611.382-.795-.075-1.814-1.02-2.271zm-18.305-3.351h-3v2h3v-2zm4 0h-3v2h3v-2z");
 
     svgEl.appendChild(path1);
     clipPlaneButton.appendChild(svgEl);
@@ -261,9 +287,10 @@ function treeButton() {
     svgEl.setAttribute("width", "15");
     svgEl.setAttribute("height", "15");
     svgEl.setAttribute("viewBox", "0 0 24 24");
+    svgEl.setAttribute("transform", "rotate(270 0 0)");
 
     const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path1.setAttribute("d", svgPaths[3]);
+    path1.setAttribute("d", "M22 18v-7h-9v-5h3v-6h-8v6h3v5h-9v7h-2v6h6v-6h-2v-5h7v5h-2v6h6v-6h-2v-5h7v5h-2v6h6v-6z");
 
     svgEl.appendChild(path1);
     treeButton.appendChild(svgEl);
@@ -271,15 +298,28 @@ function treeButton() {
     let mouseDown = false;
     //events to switch between views
     treeButton.addEventListener("click", function() {
-        if (mouseDown === false) {
-            mouseDown = true;
+        // if (mouseDown === false) {
+        //     mouseDown = true;
+        //     document.getElementById("ifc-tree-menu").style.display = "none";
+        //     treeButton.classList.remove("active");
+        // } else if (mouseDown === true) {
+        //     mouseDown = false;
+        //     document.getElementById("ifc-tree-menu").style.display = "initial";
+        //     treeButton.classList.add("active");
+        // }
+
+        if (document.getElementById("ifc-tree-menu").style.display === "initial")
+        {
             document.getElementById("ifc-tree-menu").style.display = "none";
             treeButton.classList.remove("active");
-        } else if (mouseDown === true) {
-            mouseDown = false;
+        }
+        else if (document.getElementById("ifc-tree-menu").style.display === "none")
+        {
             document.getElementById("ifc-tree-menu").style.display = "initial";
             treeButton.classList.add("active");
         }
+
+
         }, false);
 
 
@@ -298,7 +338,7 @@ function annotationsButton() {
     svgEl.setAttribute("viewBox", "0 0 24 24");
 
     const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path1.setAttribute("d", svgPaths[4]);
+    path1.setAttribute("d", "M1.438 16.873l-1.438 7.127 7.127-1.437 16.874-16.872-5.69-5.69-16.873 16.872zm1.12 4.572l.722-3.584 2.86 2.861-3.582.723zm18.613-15.755l-13.617 13.617-2.86-2.861 13.617-13.617 2.86 2.861z");
 
     svgEl.appendChild(path1);
     annotationsButton.appendChild(svgEl);
