ac384204fe20fe3c6c923f8131057516ae3f7afd
jlp <mattlukin@gmail.com>
2022-08-27 11:31:38 +0100

    clean up code

--- bundlemodellist.js
+++ bundlemodellist.js
@@ -61,15 +61,16 @@ const projects = [
   },
 ];
 
+for (let proj of projects)
+{
+    createCardDiv(proj.name, proj.id);
+   // console.log(proj.name, proj.id);
+}
+
 //get list of projects from bimserver, create a card for each project
 
 //local
 //const socket = io("http://localhost:8088/");
-//const socket = io("http://localhost:8088/");
-
-//aws
-//const socket = io("http://13.40.172.106:8088/");
-
 
 // socket.on("hello", (arg) => {
 //     console.log(arg);
@@ -77,13 +78,6 @@ const projects = [
 
 //socket.emit("getProjects", "getProjects"); //get projects from a bimserver
 
-for (let proj of projects)
-{
-    createCardDiv(proj.name, proj.id);
-   // console.log(proj.name, proj.id);
-}
-
-//need to build the projects map for locally hosted files instead of bimserver...
 // socket.on("projectIds",(resname, reslist) => {
 
 //     let projectsMap = buildMap(resname, reslist);
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import {createCardDiv} from './overlay.js';
 import { projects } from './projects.js';
 
+for (let proj of projects)
+{
+    createCardDiv(proj.name, proj.id);
+   // console.log(proj.name, proj.id);
+}
+
 //get list of projects from bimserver, create a card for each project
 
 //local
 //const socket = io("http://localhost:8088/");
-//const socket = io("http://localhost:8088/");
-
-//aws
-//const socket = io("http://13.40.172.106:8088/");
-
 
 // socket.on("hello", (arg) => {
 //     console.log(arg);
@@ -17,13 +18,6 @@ import { projects } from './projects.js';
 
 //socket.emit("getProjects", "getProjects"); //get projects from a bimserver
 
-for (let proj of projects)
-{
-    createCardDiv(proj.name, proj.id);
-   // console.log(proj.name, proj.id);
-}
-
-//need to build the projects map for locally hosted files instead of bimserver...
 // socket.on("projectIds",(resname, reslist) => {
 
 //     let projectsMap = buildMap(resname, reslist);
