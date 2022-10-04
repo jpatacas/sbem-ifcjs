d06f97375d7e567803dcc7b36b32b2f1acff49e1
jlp <mattlukin@gmail.com>
2022-10-04 12:10:09 +0100

    Upload IFC model and go to new model page
    update to index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { checkinIFC } from "./bimserver-client-upload.js";
-import { createCardDiv, buildMap, uploadCard, modelName } from "./overlay.js";
+import { createCardDiv, buildMap, uploadCard} from "./overlay.js";
 import { socket } from "./projects.js";
 
 // for (let proj of projects)
@@ -34,8 +33,7 @@ const input = document.getElementById("file-input");
 input.addEventListener(
   "change", //create project, upload file to project/bimserver
   async (changed) => {
-    //look for filw with above name, upload that file URL (hack only works for local folder)
-    //this (input stream? to DataHandler, pass DataHandler)
+
     let ifcURLlocal = input.value;
 
     let modelName = ifcURLlocal.substr(12); //get just the modelname
@@ -48,20 +46,13 @@ input.addEventListener(
 
     console.log("model name: " + modelNameNoExt);
     console.log("ifc url path: " + ifcURL);
-    //checkinIFC(ifcURL, modelName); //do this serverside, return the project id and go to that url
 
-    //socket.emit("createProject", "testproject" + Math.random())
+    socket.emit("uploadModel", modelNameNoExt, ifcURL); 
 
-    socket.emit("uploadModel", modelNameNoExt, ifcURL);
-
-    //await uploadModel(modelNameNoExt, ifcURL);
-
-    //howto use async await here??
     socket.on("newProjectData", (fileName, poid) => {
       console.log("new filename: " + fileName, "new project id: " + poid);
       window.location.href = "./bimviewer.html" + `?id=${poid}`;
 
-      //issues with existing/repeat project names??
     });
   }
 );
